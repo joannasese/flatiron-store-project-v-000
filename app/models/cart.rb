05c4e8e2b6f92b71c994370c637501212daf97b0
Donna <donnamcla@gmail.com>
2017-11-14 14:24:56 -0500

    Done.

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,23 @@
 class Cart < ActiveRecord::Base
+    has_many :line_items
+    has_many :items, through: :line_items
+    belongs_to :user
 
+    def total
+        sum = 0
+        self.line_items.each do |li|
+            sum += (li.item.price * li.quantity)
+        end
+        sum
+    end
+
+    def add_item(item_id)
+        new_li = self.line_items.find_by(id: item_id)
+        if new_li
+            new_li.quantity += 1
+        else
+            new_li = self.line_items.build(item_id: item_id)
+        end
+        new_li
+    end
 end
